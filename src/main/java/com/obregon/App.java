5c025599335de9d6d5a2e406cef2accbc12e13c0
Excy1994 <obregondoribel@gmail.com>
2021-10-22 17:00:48 -0600

    clases de builderMethod

--- src/main/java/com/obregon/App.java
+++ src/main/java/com/obregon/App.java
@@ -1,26 +1,4 @@
 package com.obregon;
 
-/**
- * Hello world!
- *
- */
-public class BarbacoaPizzaBuilder extends PizzaBuilder {
-
-    public void ponerNombre() {
-        pizza.setNombre("Barbacoa");
-    }
-    @Override
-    public void crearMasa() {
-        pizza.setMasa("gorda");
-    }
-    @Override
-    public void crearSalsa() {
-        pizza.setSalsa("barbacoa");
-    }
-    @Override
-    public void crearIngredientes() {
-        pizza.setIngredientes("mozzarella, ternera, cebolla, maíz");
-    }
+public class App {
 }
-
-
--- src/main/java/com/obregon/BarbacoaPizzaBuilder.java
+++ src/main/java/com/obregon/BarbacoaPizzaBuilder.java
@@ -1,4 +1,20 @@
 package com.obregon;
 
-public class BarbacoaPizzaBuilder {
+public class BarbacoaPizzaBuilder extends PizzaBuilder {
+
+    public void ponerNombre() {
+        pizza.setNombre("Barbacoa");
+    }
+    @Override
+    public void crearMasa() {
+        pizza.setMasa("gorda");
+    }
+    @Override
+    public void crearSalsa() {
+        pizza.setSalsa("barbacoa");
+    }
+    @Override
+    public void crearIngredientes() {
+        pizza.setIngredientes("mozzarella, ternera, cebolla, maíz");
+    }
 }

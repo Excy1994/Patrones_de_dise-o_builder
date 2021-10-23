37ad3532abfbe0dbad0b26f6e0b4ddd85fbc41bb
Excy1994 <obregondoribel@gmail.com>
2021-10-22 22:52:07 -0600

    clase CarbonaraPizzaBuilder

--- src/main/java/com/obregon/Cocinero.java
+++ src/main/java/com/obregon/Cocinero.java
@@ -1,4 +1,23 @@
 package com.obregon;
 
 public class Cocinero {
+    private PizzaBuilder pizzaBuilder;
+    public void setPizzaBuilder(PizzaBuilder pb)
+    {
+        pizzaBuilder = pb;
+    }
+
+    public Pizza getPizza()
+    {
+        return pizzaBuilder.getPizza();
+    }
+
+    public void crearPizza()
+    {
+        pizzaBuilder.crearNuevaPizza();
+        pizzaBuilder.ponerNombre();
+        pizzaBuilder.crearMasa();
+        pizzaBuilder.crearSalsa();
+        pizzaBuilder.crearIngredientes();
+    }
 }

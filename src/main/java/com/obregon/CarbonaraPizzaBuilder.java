520d93e8de704df48af16210329f4df297531e2c
Excy1994 <obregondoribel@gmail.com>
2021-10-22 22:38:43 -0600

    clase CarbonaraPizzaBuilder

--- src/main/java/com/obregon/CarbonaraPizzaBuilder.java
+++ src/main/java/com/obregon/CarbonaraPizzaBuilder.java
@@ -1,4 +1,27 @@
 package com.obregon;
 
-public class CarbonaraPizzaBuilder {
+public class CarbonaraPizzaBuilder extends PizzaBuilder {
+    @Override
+    public void ponerNombre() {
+        pizza.setNombre("Carbonara");
+
+    }
+
+    @Override
+    public void crearMasa() {
+        pizza.setMasa("fina");
+
+    }
+
+    @Override
+    public void crearSalsa() {
+        pizza.setSalsa("carbonara");
+
+    }
+
+    @Override
+    public void crearIngredientes() {
+        pizza.setIngredientes("mozzarella, bacon, cebolla");
+
+    }
 }
